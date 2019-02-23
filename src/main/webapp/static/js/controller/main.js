6a739fc18d2b40ca5f94c6134e33b85a4db20f8b
parameshkomati <47549898+parameshkomati@users.noreply.github.com>
2019-02-23 19:46:05 +0530

    Update main.js

--- src/main/webapp/static/js/controller/main.js
+++ src/main/webapp/static/js/controller/main.js
@@ -82,6 +82,11 @@ angular.module('myApp').controller('MyCtrl',['$scope',function($scope){
           $scope.vara=false;
         }
       };
-
+        var innerContents = document.getElementById(printSectionId).innerHTML;
+        var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+        popupWinindow.document.open();
+        popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+        popupWinindow.document.close();
+        });
      
 }]);
