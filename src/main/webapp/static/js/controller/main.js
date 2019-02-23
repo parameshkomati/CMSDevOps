32cf64233a96d096b3a93e06208c3d7e864380aa
parameshkomati <47549898+parameshkomati@users.noreply.github.com>
2019-02-23 23:31:22 +0530

    Update main.js

--- src/main/webapp/static/js/controller/main.js
+++ src/main/webapp/static/js/controller/main.js
@@ -82,6 +82,24 @@ angular.module('myApp').controller('MyCtrl',['$scope',function($scope){
           $scope.vara=false;
         }
       };
+    
+ 
+
+I done this way:
+
+$scope.printDiv = function (div) {
+  var docHead = document.head.outerHTML;
+  var printContents = document.getElementById(div).outerHTML;
+  var winAttr = "location=yes, statusbar=no, menubar=no, titlebar=no, toolbar=no,dependent=no, width=865, height=600, resizable=yes, screenX=200, screenY=200, personalbar=no, scrollbars=yes";
+  var newWin = window.open("", "_blank", winAttr);
+  var writeDoc = newWin.document;
+  writeDoc.open();
+  writeDoc.write('<!doctype html><html>' + docHead + '<body onLoad="window.print()">' + printContents + '</body></html>');
+  writeDoc.close();
+  newWin.focus();
+};
+
+
           $scope.PrintInfo = function() {
         var innerContents = document.getElementById("Cart").innerHTML;
         var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
